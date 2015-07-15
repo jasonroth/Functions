66215729a04eb06ce275d7f2cb0619f3047ea584
Jason <git@jasonroth.com>
2015-07-15 18:28:36 -0400

    Nat address

--- New-IHGLinuxServer.ps1
+++ New-IHGLinuxServer.ps1
@@ -17,114 +17,110 @@
     [OutputType()]
         Param (
             [Parameter(Mandatory=$true,
-            Position=0,
             ValueFromPipeline=$true,
             ValueFromPipelineByPropertyName=$true)]
-            [ValidateNotNullOrEmpty()]
-            [Alias('Name')]
-            [string]
+			[ValidateNotNullOrEmpty()]
+			[string]
             $ComputerName,
             
             [Parameter(Mandatory=$true,
-            Position=1,
-            ValueFromPipeline=$true,
+            #ValueFromPipeline=$true,
             ValueFromPipelineByPropertyName=$true)]
-            [ValidateNotNullOrEmpty()]
-            [Alias('Forest')]
-            [string]
+			[ValidateNotNullOrEmpty()]
+			[string]
             $Domain,
 
             [Parameter(Mandatory=$true,
-            Position=2,
             ValueFromPipeline=$true,
             ValueFromPipelineByPropertyName=$true)]
-            [ValidateNotNullOrEmpty()]
-            [Alias('Zone')]
-            [string]
+			[ValidateNotNullOrEmpty()]
+			[string]
             $CentrifyZone,
 
             [Parameter(Mandatory=$true,
-            Position=3,
             ValueFromPipeline=$true,
             ValueFromPipelineByPropertyName=$true)]
-            [ValidateNotNullOrEmpty()]
-            [Alias('OU')]
-            [string]
+			[ValidateNotNullOrEmpty()]
+			[string]
             $Path,
 
             [Parameter(Mandatory=$true,
-            Position=4,
             ValueFromPipeline=$true,
             ValueFromPipelineByPropertyName=$true)]
-            [ValidateNotNullOrEmpty()]
-            [string]
+			[ValidateNotNullOrEmpty()]
+			[string]
             $DataCenter,
 
             [Parameter(Mandatory=$true,
-            Position=5,
             ValueFromPipeline=$true,
             ValueFromPipelineByPropertyName=$true)]
-            [ValidateNotNullOrEmpty()]
-            [Alias('SecGroup')]
-            [string]
+			[ValidateNotNullOrEmpty()]
+			[string]
             $Group,
 
             [Parameter(Mandatory=$true,
-            Position=6,
             ValueFromPipeline=$true,
             ValueFromPipelineByPropertyName=$true)]
-            [ValidateNotNullOrEmpty()]
-            [Alias('IP')]
-            [string]
-            $IPv4Address
+			[ValidateNotNullOrEmpty()]
+			[string]
+            $IPv4Address,
+
+            [Parameter(Mandatory=$true,
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true)]
+			[ValidateNotNullOrEmpty()]
+			[string]
+            $NatAddress
         )
 
-    Begin {
+    Begin {}
+    Process {
+        foreach ($Name in $ComputerName) {
 
 #Discover domain controller in target forest and site
-        try {
-            Get-ADDomainController -Discover -DomainName $Domain -SiteName $DataCenter -Writable -OutVariable 'ADServer' -ErrorAction Stop
-            Set-CdmPreferredServer -Domain $Domain -Server $ADServer.HostName -ErrorAction Stop
-        }
-        catch {
-            throw $_.Exception.Message
-        }
-    }
-    Process {
+            try {
+                Get-ADDomainController -Discover -DomainName $Domain -SiteName $DataCenter -Writable -OutVariable 'ADServer'
+                Set-CdmPreferredServer -Domain $Domain -Server $ADServer.HostName
+            }
+            catch {
+                throw $_.Exception.Message
+            }
 
 #Add computer object to AD and provision in Centrify
-        try {
-            New-CdmManagedComputer -Name $ComputerName -Zone $CentrifyZone -Container $Path
-        }
-        catch {
-            throw $_.Exception.Message
-        }
+            try {
+                New-CdmManagedComputer -Name $ComputerName -Zone $CentrifyZone -Container $Path -ErrorAction Continue
+            }
+            catch {
+                throw $_.Exception.Message
+            }
     
 #Wait for computer object to be discoverable in AD or timeout after 60 seconds
-        if (-Not( Get-ADComputer -Server $ADServer.HostName -Filter {Name -eq $ComputerName})) {
-            $i = 0
-            do {
-                Wait-Event -Timeout 10
-                $i++
+            if (-Not( Get-ADComputer -Server $ADServer.HostName -Filter {Name -eq $ComputerName})) {
+                $i = 0
+                do {
+                    Wait-Event -Timeout 10
+                    $i++
+                }
+                until ((Get-ADComputer -Server $ADServer.HostName -Filter {Name -eq $ComputerName}) -or $i -eq 6)   
             }
-            until ((Get-ADComputer -Server $ADServer.HostName -Filter {Name -eq $ComputerName}) -or $i -eq 6)   
-        }
 
 #Add computer object to AD security group
-        try {
-            Add-ADGroupMember -Identity $Group -Members ($ComputerName+'$') -Server $ADServer.HostName -ErrorAction Stop
-        }
-        catch {
-            throw $_.Exception.Message
-        }
+            try {
+                Add-ADGroupMember -Identity $Group -Members ($ComputerName+'$') -Server $ADServer.HostName
+            }
+            catch {
+                throw $_.Exception.Message
+            }
     
-#Add DNS Record
-        try {
-             Add-DnsServerResourceRecordA -ComputerName $ADServer.HostName -CreatePtr -IPv4Address $IPv4Address -Name $ComputerName -ZoneName $Domain
+#Add DNS Records
+            try {
+                 Add-DnsServerResourceRecordA -ComputerName $ADServer.HostName -CreatePtr -IPv4Address $IPv4Address -Name $ComputerName -ZoneName $Domain
+                 Add-DnsServerResourceRecordA -ComputerName $ADServer.HostName -CreatePtr -IPv4Address $NatAddress -Name $ComputerName-nat -ZoneName $Domain
+            }
+            catch {
+                throw $_.Exception.Message
+            }
         }
-        catch {
-            throw $_.Exception.Message
-        }
-    }
+	}
     End {}
 }
