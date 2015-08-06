7544933419cc4d895b13d037fe666e3286c99dee
Jason <git@jasonroth.com>
2015-08-06 18:19:37 -0400

    cleanup

--- Update-CloudAttribute10.ps1
+++ Update-CloudAttribute10.ps1
@@ -1,5 +1,6 @@
-﻿$Users = Get-ADUser -Server apac.corp.local -Filter {Name -like 'admin_*'} -Properties msDS-cloudExtensionAttribute10, CanonicalName #| Select UserPrincipalName, msDS-cloudExtensionAttribute10, CanonicalName
-$Cred = Get-Credential corp\adminrt_rothja
+﻿$Cred = Get-Credential corp\adminrt_rothja
+$Users = Get-ADUser -Server corp.local:3268 -Filter {enabled -eq 'True'} -Properties msDS-cloudExtensionAttribute10, CanonicalName
+$Log = C:\_limbo\Attrib10_Update.txt
 
 foreach ($User in $Users) {
     $Properties = [ordered] @{
@@ -9,14 +10,11 @@ foreach ($User in $Users) {
         NewAttrib10 = 'CN='+$User.SamAccountName+'/O='+$User.CanonicalName.split(".")[0]
         Domain = $User.CanonicalName.split(".")[0]
     }
-    #$NewAttrib10 = 'CN='+$Properties.Sam+'/O='+$Properties.Domain
-    #$Properties.Add('NewAttrib10', $NewAttrib10)
-    $Object = New-Object -TypeName psObject -Property $Properties
-    Write-Output $Object
+    
     if ($User.'msDS-cloudExtensionAttribute10' -ne $Object.NewAttrib10) {
         Write-Host "Updating attribute on $($Object.UPN) from $($Object.Attrib10) to $($Object.NewAttrib10)"
+        $Object | Out-File $Log
         $Domain = $Object.UPN.Split('@')[1]
         Set-ADUser -Server $Domain -Identity $User -Replace @{'MSDS-CloudExtensionAttribute10'=$Object.NewAttrib10} -Credential $Cred
     }
-    
 }
