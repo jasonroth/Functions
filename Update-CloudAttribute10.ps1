eb6e661933666970507e29bf611d939a7406a73c
Jason <git@jasonroth.com>
2015-08-07 13:47:39 -0400

    fix issue w/ object

--- Update-CloudAttribute10.ps1
+++ Update-CloudAttribute10.ps1
@@ -1,6 +1,6 @@
-﻿$Cred = Get-Credential corp\adminrt_rothja
-$Users = Get-ADUser -Server corp.local:3268 -Filter {enabled -eq 'True'} -Properties msDS-cloudExtensionAttribute10, CanonicalName
-$Log = C:\_limbo\Attrib10_Update.txt
+﻿#$Cred = Get-Credential corp\adminrt_rothja
+#$Users = Get-ADUser -Server corp.local:3268 -Filter {enabled -eq 'True'} -Properties msDS-cloudExtensionAttribute10, CanonicalName
+$Log = 'C:\_limbo\Attrib10_Update.csv'
 
 foreach ($User in $Users) {
     $Properties = [ordered] @{
@@ -11,10 +11,15 @@ foreach ($User in $Users) {
         Domain = $User.CanonicalName.split(".")[0]
     }
     
-    if ($User.'msDS-cloudExtensionAttribute10' -ne $Object.NewAttrib10) {
-        Write-Host "Updating attribute on $($Object.UPN) from $($Object.Attrib10) to $($Object.NewAttrib10)"
-        $Object | Out-File $Log
-        $Domain = $Object.UPN.Split('@')[1]
-        Set-ADUser -Server $Domain -Identity $User -Replace @{'MSDS-CloudExtensionAttribute10'=$Object.NewAttrib10} -Credential $Cred
+    $Object = New-Object -TypeName psObject -Property $Properties
+    if ($Object.UPN -ne $null) {
+        if ($User.'msDS-cloudExtensionAttribute10' -ne $Object.NewAttrib10) {
+            Write-Host "Updating attribute on $($Object.UPN) from $($Object.Attrib10) to $($Object.NewAttrib10)"
+            $Object | export-csv -NoTypeInformation $Log -Append
+            $Domain = $Object.Domain
+            Set-ADUser -Server $Domain -Identity $User -Replace @{'MSDS-CloudExtensionAttribute10'=$Object.NewAttrib10} -Credential $Cred
+        }
     }
+    Remove-Variable Object
+    Remove-Variable Properties
 }
