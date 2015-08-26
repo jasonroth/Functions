2e8217f94a9da3c7b576451016932528a9974a4b
Jason <git@jasonroth.com>
2015-08-26 09:30:17 -0400

    New

---
+++ Set-IHGUserName.ps1
@@ -0,0 +1,50 @@
+﻿Function Set-IHGUserName {
+    <#
+        .SYNOPSIS
+            ####
+
+        .DESCRIPTION
+            ####
+
+        .PARAMETER Name
+            ####
+        
+        .EXAMPLE
+            ####
+        
+		.EXAMPLE
+            ####
+			
+        .EXAMPLE
+            ####
+  #>
+
+    [CmdletBinding()]
+        Param (
+            [Parameter(Mandatory=$true,
+            Position=0,
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true)]
+			[ValidateNotNullOrEmpty()]
+			[string[]]
+            $Identity
+        )
+
+    Begin {}
+    Process {
+        foreach ($User in $Identity) {
+            $Domain = ($User.UserPrincipalName).split('@')[1]
+            $DisplayName = "$User.Surname"+", "+"$User.GivenName"
+            Set-ADUser -Server $Domain -Identity $User.UserPrincipalName -DisplayName $DisplayName
+                       
+            [psobject]@{
+                Name = $User.Name
+                UserPrincipalName = $User.UserPrincipalName
+                PreviousDisplayName = $User.DisplayName
+                NewDisplayName = $DisplayName
+            }
+        }
+
+	}
+    End {}
+}
