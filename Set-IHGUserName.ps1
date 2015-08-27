4a257664a807a7bc766765c6b4b7d9c422a968aa
Jason <git@jasonroth.com>
2015-08-27 14:29:43 -0400

    inline help

--- Set-IHGUserName.ps1
+++ Set-IHGUserName.ps1
@@ -1,35 +1,42 @@
 ﻿# Set earliest date for user creation
+
 $Date = [datetime]'06/01/2013'
 
 # Search for enabled accounts created since $Date
-$Users = Get-IHGUser -Name test* -Enabled | where Created -GT $Date
+$Users = Get-IHGUser -Enabled | where Created -GT $Date
 
 # Configure Logging
+
 $LogPath = "$env:SystemDrive\logs\Change_DisplayName"
 $LogFile = (Get-Date -Format yyyy_MM_dd)+"_Change_DisplayName.csv"
 $ErrorLog = (Get-Date -Format yyyy_MM_dd)+"_Change_DisplayName_Errors.log"
 
 # Create logging directory
+
 if (-not (Test-Path $LogPath)) {
     New-Item -ItemType Directory -Path $LogPath -Force | Out-Null
 }
         
 # Iterate through user accounts
+
 foreach ($User in $Users) {
     
     # Create variables to use with Set-ADUser commandlet
+
     $Domain = ($User.UserPrincipalName).split('@')[1]
     try {
         $NewDisplayName = $User.Surname.Trim()+", "+$User.GivenName.Trim()
     }
     catch {
         # Write Failures to error log
+
         $Message = (Get-Date -Format HH:mm:ss).ToString()+" : Unable to determine new DisplayName for $User"
         Write-Verbose $Message
         $Message | Out-File -Append -FilePath $LogPath\$ErrorLog
     }
     
     # Change the DisplayName if it does not currently start with "LastName, FirstName"
+
     if ($User.DisplayName -notlike $NewDisplayName+'*') {
         try {
             Set-ADUser -Server $Domain -Identity $User.SamAccountname -DisplayName $NewDisplayName -Verbose -WhatIf
@@ -37,12 +44,14 @@ foreach ($User in $Users) {
         catch {
            
            # Write Failures to error log
+
            $Message = (Get-Date -Format HH:mm:ss).ToString()+" : Failed to set DisplayName for $($User.UserPrincipalName)"
            Write-Verbose $Message
            $Message | Out-File -Append -FilePath $LogPath\$ErrorLog 
         }
         
         #Create custom object to export to log
+
         $Object = [PSCustomObject] @{
             UserPrincipalName = $User.UserPrincipalName
             PreviousDisplayName = $User.DisplayName
@@ -50,7 +59,8 @@ foreach ($User in $Users) {
             Created = $User.Created
         }
         
-        # Export custom object to log, then clear object variable    
+        # Export custom object to log, then clear object variable
+
         $Object | Export-Csv -NoTypeInformation -Append -Path $LogPath\$LogFile
         Remove-Variable Object
     }
